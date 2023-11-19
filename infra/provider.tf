5e88117abb6d2959ef973f61a612b03961da2464
HauVu94 <hau_vu94@hotmail.com>
2023-11-19 17:04:58 +0000

    håper det funker nå oppgave 3

---
+++ infra/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.25.0"
+    }
+  }
+    backend "s3" {
+    bucket = "pgr301-2021-terraform-state"
+    key    = "kandidatnr-2007/apprunner-a-new-state.state"
+    region = "eu-north-1"
+  }
+}
